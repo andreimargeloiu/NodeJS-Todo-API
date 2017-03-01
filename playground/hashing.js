04e939ab40340c730a11f5090ca6cbcf281cb31a
margiki <andrei.margeloiu@gmail.com>
2017-03-01 16:39:11 +0000

    added user tokens

---
+++ playground/hashing.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+
+// jwt.sign
+// jwt.verify
+
+var data = {
+    id: 10
+};
+
+// String <- jwt.sign(object, 'someRandomSecret');
+var token = jwt.sign(data, '123abs'); // face un token (String), care are informatia
+                                      // despre data cat si hash-ul ei
+
+// Object <- jwt.verify(String, 'someRandomSecret');                          
+var decode = jwt.verify(token, '123abs'); // retruneaza obiectul intial care a fost codat
+console.log(decode);
+
+
+
+// const {SHA256} = require('crypto-js');
+//
+// var message = 'I am user number 3';
+// var hash = SHA256(message).toString();
+//
+// var data = {
+//     id: 4
+// }
+//
+// var token = {
+//     data,
+//     hash: SHA256(JSON.stringify(data) + 'someRandomSecret').toString()
+// }
+//
+// var newhash = SHA256(JSON.stringify(token.data) + 'someRandomSecret').toString();
+//
+// if (newHash === token.hash) {
+//     console.log("YES, they are the same");
+// } else {
+//     console.log('NO, they are not the same');
+// }
--- server/server.js
+++ server/server.js
@@ -117,6 +117,34 @@ app.patch('/todos/:id', (req, res) => {
 	});
 });
 
+// put a new user in the database
+app.post('/users', (req, res) => {
+    var userAux = _.pick(req.body, ['email', 'password'])
+    var user = new User(userAux);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
+
+// app.post('/todos', (req, res) => {
+//   var todo = new Todo({
+//     text: req.body.text
+//   });
+//
+//   todo.save().then((doc) => {
+//     res.send(doc);
+//   }, (err) => {
+//     res.status(400).send(err);
+//   });
+// });
+
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
